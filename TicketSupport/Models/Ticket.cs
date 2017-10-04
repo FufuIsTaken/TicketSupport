a73137d73f733582a832c66c87633657092f03a6
Furat Wushel <Furat Wushel>
2017-10-04 11:37:26 +0200

    Add project files.

---
+++ TicketSupport/Models/Ticket.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TicketSupport.Models
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Customer { get; set; }
+        [Required]
+        public string Application { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public string Status { get; set; }
+
+    }
+}
